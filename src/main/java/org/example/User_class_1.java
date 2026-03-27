4393d40f3271e0515e71b1f78f67e9b66d7a583a
Jeevika174 <tiyaapap@gmail.com>
2026-03-27 10:14:03 +0530

    UC2

--- src/main/java/org/example/User_class_1.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-
-public class User_class_1 {
-    public static void main(String[] args){
-        System.out.println("Hello World!");
-    }
-}
---
+++ src/main/java/org/example/User_class_2.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class User_class_2 {
+    public void main(String[] args){
+        String name = args[0];
+        System.out.println("Hello," + name + "!");
+    }
+}
